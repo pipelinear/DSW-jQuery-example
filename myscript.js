3d143530ae0b61ebc3b8b087281c9b1d228dae86
iderisadev <ider.kawabata@gmail.com>
2026-09-04 13:38:04 -0700

    Update jQuery example

--- myscript.js
+++ myscript.js
@@ -6,4 +6,23 @@ $(document).ready(function() {
 		$("p").addClass("bright");
 		$("img").addClass("border");
 	});
+	
+	$("#p1").click(function() {
+		$(".disappear").toggle(); //or hide
+	});
+	
+	
+	$(".disappear").hover(function(){
+  alert("bro click me bro");
+},
+function(){
+  alert("also im a action fyi");
 });
+	
+	
+});
+
+//
+//In myScript.js, add code to make it so clicking on the first paragraph makes
+//the second and third paragraphs disappear. 
+//Hint: To select all elements in a class, use (.className) as the selector. 
